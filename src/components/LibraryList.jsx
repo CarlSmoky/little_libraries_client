480fccc589a7d9680dc2419050895e9d44ebaaf7
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-25 16:31:53 -0400

    Add fetch libraries

--- src/components/LibraryList.jsx
+++ src/components/LibraryList.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function LibraryList() {
+const LibraryList = () => {
+  const [mostRecentlyVisitedLibrary, setMostRecentlyVisitedLibrary] = useState([]);
+
+  const [mostFrequentlyVisitedLibrary, setmostFrequentlyVisitedLibrary] = useState([]);
+
+
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const headers = { "x-access-token": token, };
+    const endpoints = {
+      "USER": `api/users/libraries`
+    }
+    axios.post(endpoints.USER, '', { headers })
+      .then(response => {
+        const { MostRecentlyVisitedLibrariesForUser, MostFrequentlyVisitedLibrariesForUser } = response.data;
+        setMostRecentlyVisitedLibrary(MostRecentlyVisitedLibrariesForUser);
+        setmostFrequentlyVisitedLibrary(MostFrequentlyVisitedLibrariesForUser)
+      })
+      .catch(err => {
+        console.log(err.response.data);
+      });
+
+  }, []);
+
   return (
-    <div>LibraryList</div>
+    <div>
+      <h2>LibraryList</h2>
+    </div>
   )
 }
 
