f7270cc33ded416ed519be93a98195ff1e092c6c
tsumitakaoru <k.tsumita01@gmail.com>
2022-06-02 16:33:08 -0400

    Remove console.log

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -24,7 +24,7 @@ const AuthProvider = props => {
   const firebaseSignOut = () => {
     const auth = getAuth();
     signOut(auth).then(() => {
-      console.log("firebase logged out");
+      // console.log("firebase logged out");
     }).catch((error) => {
       console.log("firebase failed to log out");
     });
