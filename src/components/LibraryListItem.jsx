5727f995251b1a5a221d23da095d674992e6efd5
tsumitakaoru <k.tsumita01@gmail.com>
2022-06-10 12:42:29 -0400

    Add icon and margin

--- src/components/LibraryListItem.jsx
+++ src/components/LibraryListItem.jsx
@@ -9,9 +9,9 @@ const LibraryListItem = (props) => {
         {props.imageUrl && <img className="card_image" src={props.imageUrl} alt='little library image' />}
         {!props.imageUrl && <img className="card_image no-image" src='https://firebasestorage.googleapis.com/v0/b/little-libraries-ea3cb.appspot.com/o/images%2F1.jpg?alt=media&token=008505ad-0024-420c-888f-9f8b8f707efa' alt='little library image' />}
         <div class="text-container">
-          <h4><b>{props.address}</b></h4>
-          <p>{props.count} visits</p>
-          {props.lastVisited && <p>Last visit: {formatDateFromSQL(props.lastVisited)} </p>}
+          <h3><b>{props.address}</b></h3>
+          <p><i className="fas fa-street-view"></i> {props.count} visits</p>
+          {props.lastVisited && <p><i className="fas fa-calendar-check"></i> Last visit: {formatDateFromSQL(props.lastVisited)} </p>}
         </div>
       </div>
     </Link>
